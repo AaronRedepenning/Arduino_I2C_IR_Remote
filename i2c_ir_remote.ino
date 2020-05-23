cce16e909c65dfec8946903ddd181b4613c624ac
Aaron Redepenning <aaronredepenning@live.com>
2020-05-23 10:13:29 -0500

    Switch to Infrared4Arduino library for Arduino Nano Every support

--- i2c_ir_remote.ino
+++ i2c_ir_remote.ino
@@ -6,9 +6,29 @@
  *******************************************************/
 
 #include <Wire.h>
-#include <IRremote.h>
+#include <Pronto.h>
+#include <IrSenderPwm.h>
 
 #define cnt_of_array(x) (sizeof(x) / sizeof(0[x]))
+
+/*
+ * Pin to use for PWM
+ * IrSenderPwm only supports pin 6 on Arduino Nano Every
+ */
+#define PIN ( 6 )
+
+/*
+ * Debug macros
+ */
+#define DEBUG ( 0 )
+
+#if DEBUG
+  #define DBG_PRINT(...)    Serial.print(__VA_ARGS__)
+  #define DBG_PRINTLN(...)  Serial.println(__VA_ARGS__)
+#else 
+  #define DBG_PRINT(...)
+  #define DBG_PRINTLN(...)
+#endif
 
 /*
  * I2C Slave Address
@@ -50,23 +70,23 @@ const PROGMEM char volume_down_code[]  = "0000 006C 0000 0022 00AD 00AD 0016 004
  */
 typedef struct
   {
-    i2c_command_t i2c_command;
-    const char *  ir_code;
+    i2c_command_t       i2c_command;
+    const char *        ir_code;
+    const unsigned int  n_sends;
   } command_map_t;
 
 const command_map_t command_map[] = 
   {
-    /* I2C Command                IR Code           */
-    { I2C_COMMAND_POWER         , power_toggle_code },
-    { I2C_COMMAND_CHANNEL_UP    , channel_up_code   },
-    { I2C_COMMAND_CHANNEL_DOWN  , channel_down_code },
-    { I2C_COMMAND_VOLUME_UP     , volume_up_code    },
-    { I2C_COMMAND_VOLUME_DOWN   , volume_down_code  }
+    /* I2C Command                IR Code             Number of times to send IR code */
+    { I2C_COMMAND_POWER         , power_toggle_code,  1                               },
+    { I2C_COMMAND_CHANNEL_UP    , channel_up_code,    1                               },
+    { I2C_COMMAND_CHANNEL_DOWN  , channel_down_code,  1                               },
+    { I2C_COMMAND_VOLUME_UP     , volume_up_code,     3                               },
+    { I2C_COMMAND_VOLUME_DOWN   , volume_down_code,   3                               }
 
     /* Add new i2c -> ir command mappings here */
   };
   
-IRsend          ir_send;
 i2c_command_t   recieved_command;
 
 /*
@@ -93,42 +113,27 @@ void loop()
 {
   if (I2C_COMMAND_VALID(recieved_command))
   {
-    int i;
-    
-    for (i = 0; i < cnt_of_array(command_map); i++)
+    for (int i = 0; i < cnt_of_array(command_map); i++)
     {
       if (command_map[i].i2c_command == recieved_command)
       { 
-        static unsigned int code[256];
-        static int          len;
-        static int          freq;
-
         DBG_PRINT(F("Command received: "));
         DBG_PRINTLN(recieved_command);
 
-        convertProntoProgmem(command_map[i].ir_code, code, &len, &freq);
-        
-        DBG_PRINT(F("Pronto Code: "));
-        DBG_PRINTLN(command_map[i].ir_code);
-        
-        DBG_PRINT(F("Raw("));
-        DBG_PRINT(len);
-        DBG_PRINT(F("): "));
-        for (int i = 0; i < len; i++)
+        const IrSignal * irSignal = Pronto::parse_PF(command_map[i].ir_code);
+        #if DEBUG
+          irSignal->dump(Serial, true);
+        #endif
+
+        IrSender * irSender = IrSenderPwm::getInstance(true, PIN);
+        for (int n = 0; n < command_map[i].n_sends; n++)
         {
-          DBG_PRINT(code[i]);
-          DBG_PRINT(" ");
+          irSender->sendIrSignal(*irSignal, 1);
+          delay(100);
         }
-        DBG_PRINTLN("")
-        
-        DBG_PRINT(F("Frequency: "));
-        DBG_PRINTLN(freq);
-        
-        ir_send.sendRaw(code, len, freq);
         break; 
       }
     }
-
     recieved_command = I2C_COMMAND_INVALID;
   }
 }
@@ -154,67 +159,4 @@ void receiveEvent(int bytesReceived)
       (void)Wire.read();
     }
   }
-}
-
-/*
- * Function: convertProntoProgmem()
- * Description: Convert a pronto hex string, stored in FLASH, to code length frequency
- *              values required by the IRremote library
- */
-void convertProntoProgmem(PGM_P i_pronto, unsigned int * o_code, int * o_length, int * o_frequency)
-{
-  int       i = 0;
-  uint16_t  j = 0;
-  uint16_t  arr[128];
-
-  while (i < strlen_P(i_pronto))
-  {
-    char hexchararray[5];
-    strncpy_P(hexchararray, i_pronto + i, 4);
-    hexchararray[4] = '\0';
-
-    unsigned int hexNumber = hexToInt(hexchararray);
-    arr[j++] = hexNumber;
-    i = i + 5;
-  }
-
-  float carrierFrequency = 1000000 / (arr[1] * 0.241246);
-
-  int codeLength = arr[2];
-  if (codeLength == 0) codeLength = arr[3];
-
-  int repeatCodeLength = arr[3];
-  *o_length = codeLength*2;
-
-  int index = 0;
-  
-  for (int i = 4; i < j; i++ )
-  {
-    int convertedToMicrosec = (1000000 * (arr[i] / carrierFrequency) + 0.5);
-    o_code[index++] = convertedToMicrosec;
-  }
-
-  *o_frequency = (int)(carrierFrequency / 1000);
-}
-
-/*
- * Function: hexToInt()
- * Description: Convert hex string in integer. Ported from:
- *               http://stackoverflow.com/questions/4951714/c-code-to-convert-hex-to-int
- */
-unsigned int hexToInt(const char *hex)
-{
-  unsigned int result = 0;
-  while (*hex)
-  {
-    if (*hex > 47 && *hex < 58)
-      result += (*hex - 48);
-    else if (*hex > 64 && *hex < 71)
-      result += (*hex - 55);
-    else if (*hex > 96 && *hex < 103)
-      result += (*hex - 87);
-    if (*++hex)
-      result <<= 4;
-  }
-  return result;
 }
